c2f14507a02a256eeb62ef8bbb76cbf5c149746a
Thean <theantian089@gmail.com>
2024-07-14 16:56:20 +0700

    chore: Remove scopedStyleStrategy from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  scopedStyleStrategy: "where",
   output: "hybrid",
   adapter: vercel(),
   image: {
