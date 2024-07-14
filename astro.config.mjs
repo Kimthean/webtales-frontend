8441e8c9e21a089f099833dcef4824a604c3c39c
Thean <theantian089@gmail.com>
2024-07-14 17:13:02 +0700

    feat: Add lazy loading for images in NovelCard and novel detail page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,22 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
   image: {
-    remotePatterns: [{ protocol: "http", hostname: "img.9999txt.cc" }],
-      domains: ['img.9999txt.cc'],
+    domains: ['img.9999txt.cc'],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "img.9999txt.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "img.9999txt.cc",
+      }
+    ],
   },
   vite: {
     ssr: {
