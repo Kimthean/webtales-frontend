077cfa39b853a8b59f79866c20eb6db1c2b5d462
Thean <theantian089@gmail.com>
2024-07-17 14:51:55 +0700

    add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,33 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://webtales-eight.vercel.app',
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
   }),
   image: {
     service: passthroughImageService(),
     domains: ['img.9999txt.cc'],
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "img.9999txt.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "img.9999txt.cc",
-      }
-    ],
+    remotePatterns: [{
+      protocol: "http",
+      hostname: "img.9999txt.cc"
+    }, {
+      protocol: "https",
+      hostname: "img.9999txt.cc"
+    }]
   },
   vite: {
     ssr: {
       external: ['@11ty/eleventy-img', 'svgo']
     }
   },
-  trailingSlash: 'always',
+  trailingSlash: 'always'
 });
