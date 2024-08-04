fc50413c20dd3065200fa6732de3f9e192e3c386
Thean <theantian089@gmail.com>
2024-08-04 21:09:15 +0700

    remove vercel analyti

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,33 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://webtales-eight.vercel.app',
+  site: "https://webtales-eight.vercel.app",
   integrations: [tailwind(), icon(), react()],
   output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
+  adapter: vercel(),
   image: {
     service: passthroughImageService(),
-    domains: ['img.9999txt.cc'],
-    remotePatterns: [{
-      protocol: "http",
-      hostname: "img.9999txt.cc"
-    }, {
-      protocol: "https",
-      hostname: "img.9999txt.cc"
-    }]
+    domains: ["img.9999txt.cc"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "img.9999txt.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "img.9999txt.cc",
+      },
+    ],
   },
   vite: {
     ssr: {
-      external: ['@11ty/eleventy-img', 'svgo']
-    }
+      external: ["@11ty/eleventy-img", "svgo"],
+    },
   },
-  trailingSlash: 'always'
+  trailingSlash: "always",
 });
