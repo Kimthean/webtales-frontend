32d454e293b5e6d93cc1dabd33d8bb8944c2a6e5
Thean <theantian089@gmail.com>
2024-09-05 22:10:50 +0700

    refactor: change render strategy for bookmarkbutton

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,32 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercelServerless from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
-import sitemap from '@astrojs/sitemap';
-import preact from '@astrojs/preact';
+import sitemap from "@astrojs/sitemap";
+import preact from "@astrojs/preact";
 
 import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://webtalesmtl.xyz",
-  integrations: [tailwind(), icon(), sitemap(), preact({
-    compat: true
-  }), auth()],
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap(),
+    preact({
+      compat: true,
+    }),
+    auth(),
+  ],
   output: "server",
   adapter: vercelServerless(),
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"]
+      external: ["@11ty/eleventy-img", "svgo"],
     },
     server: {
-      hmr: false
-    }
+      hmr: false,
+    },
   },
-  trailingSlash: "never"
+  trailingSlash: "never",
 });
